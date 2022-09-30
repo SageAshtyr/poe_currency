8daf39721297fc8158ad831a343d5b07a59ff99e
Michael Ashtyr <110384934+SageAshtyr@users.noreply.github.com>
2022-09-30 08:48:15 +0300

    Back to working version of scraper

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+import requests
+import pandas as pd
+import json
+import asyncio
+
+
+def get_data():
+    url = "https://poe.ninja/api/data/currencyoverview?league=Kalandra&type=Currency"
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Request to Poe.Ninja failed, most likely invalid league provided")
+        sys.exit(1)
+    return json.loads(response.text)["lines"]
+
+
+def get_cor_pandas(lines: dict):
+    pds = pd.DataFrame.from_dict(lines)[['currencyTypeName', 'chaosEquivalent']]
+    return pds
+
+
+def save_pandas(pds):
+    pds.to_csv('poe_currency.csv', index=True)
+
+
+save_pandas(get_cor_pandas(get_data()))
+# print(get_corr_pandas(get_data()))
